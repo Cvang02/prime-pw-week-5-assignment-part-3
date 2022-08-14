ade8606e02005a0d9fff94126a8ee02b34a99c57
Chameng Vang <Chameng.vang@outlook.com>
2022-08-14 17:22:03 -0500

    recode search/find artists function and added tracks to show collections

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,16 +10,17 @@ let collection = [];
 //   - Add the new object to the end of the `collection` array
 //   - Return the newly created object
 
-function addToCollection(title, artist, yearPublished) {
-    const album = {
+function addToCollection(title, artist, yearPublished, tracks) {
+    const albums = {
         title ,
         artist ,
-        yearPublished
+        yearPublished ,
+        tracks: [tracks]
     }
-    collection.push(album)
-    return album;
+    collection.push(albums)
+    return albums;
 }
-   
+
 // - Test the `addToCollection` function:
 //   - Add 6 albums to your collection. Aim to have a mix of both 
 // same and different artists and published years. 
@@ -27,13 +28,37 @@ function addToCollection(title, artist, yearPublished) {
 //   - Console.log each album as added using the returned value.
 //   - After all are added, console.log the `collection` array.
 
-console.log(addToCollection('Life Goes On','BTS',2020));
-console.log(addToCollection('Make It Right','BTS', 2019));
-console.log(addToCollection('Spring Day','BTS', 2017));
-console.log(addToCollection('Playing with Fire','BlackPink',2017));
-console.log(addToCollection('Kill This Love','BlackPink',2019));
-console.log(addToCollection('Lovesick Girls','BlackPink',2020));
-console.log(collection);
+console.log(addToCollection('Be','BTS',2020,[{
+    track1: {
+        trackName: 'Life Goes On',
+        duration: '3:28'
+    },
+    track2: {
+        trackName: 'Stay',
+        duration: '3:25'
+    },
+    track3: {
+        trackName: 'Dynamite',
+        duration: '3:19'
+    }
+}]));
+
+console.log(addToCollection('MAP OF THE SOUL: PERSONA ','BTS', 2019,[{
+    track1: {
+        trackName:'Boy with LUV',
+        duration: '3:50'},
+    track2: {
+        trackName: 'Make It Right',
+        duration: '3:26'
+    }
+}]));
+
+console.log(addToCollection('You Never Walk Alone','BTS', 2017,['N/A']));
+console.log(addToCollection('Square two','BlackPink',2017,['N/A']));
+console.log(addToCollection('Kill This Love','BlackPink',2019,['N/A']));
+console.log(addToCollection('The Album','BlackPink',2020,['N/A']));
+
+
 
 // - Add a function named `showCollection`. This function should:
 //   - Take in an array parameter. (This allows it to be reused to show any collection, 
@@ -43,17 +68,15 @@ console.log(collection);
 // formatted like: `TITLE by ARTIST, published in YEAR`.
 // - Test the `showCollection` function.
 
+console.log ('Albums Information formatted to Title by Artist, published in year: plus tracks!!!');
+
 function showCollection (array) {
-    console.log('The number of items in the array', array.length);
-    for (let i=0; i<array.length; i++);
-    console.log (array[0].title, 'by', array[0].artist, 'published in', array[0].yearPublished);
-    console.log (array[1].title, 'by', array[1].artist, 'published in', array[1].yearPublished);
-    console.log (array[2].title, 'by', array[2].artist, 'published in', array[2].yearPublished);
-    console.log (array[3].title, 'by', array[3].artist, 'published in', array[3].yearPublished);
-    console.log (array[4].title, 'by', array[4].artist, 'published in', array[4].yearPublished);
-    console.log (array[5].title, 'by', array[5].artist, 'published in', array[5].yearPublished);
-    return collection;
-}
+    for (i=0; i<array.length; i++) {
+        console.log (array[i].title, 'by', array[i].artist, ',published in', array[i].yearPublished,
+        ' - Including title tracks:', array[i].tracks);
+    } // end of for-loop.
+ return collection;
+} //end of showCollection-function.
 
 console.log(showCollection(collection));
 
@@ -67,42 +90,60 @@ console.log(showCollection(collection));
 //  as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are found.
 // > When testing your functions, write all tests in the JavaScript file!
 
+function findByArtist(artist) {
+    let findArtist = [];
+      for (let singers of collection) {
+        if (singers.artist === artist) {
+        findArtist.push(singers);
+        } // end of if-statement.
+        // else if ( singers.artist !== artist ) {
+        // findArtist.push('no match');     
+        // }    
+    } // end of for-loop.
+    return findArtist;
+} // end of findByArtist-function.
 
-function findByArtist (artist) {
-    let anArray = [];
-    let singers = 'BTS'
-    for (let i=0; i<artist.length; i++) {
-        if (artist[i].artist === singers) {
-            console.log(artist[i]);
-            
-        }
-        else {
-            return [];
-        }
-    }
-}
-
-console.log(findByArtist (collection));
-// console.log(findByArtist ('BTS'));
-// console.log(findByArtist ('BLackPink'));
+console.log('Should Show 3 Album from BTS:',findByArtist('BTS'));
+console.log('Should Show 3 Album from BlackPink:',findByArtist('BlackPink'));
+console.log('No Matched Founded',findByArtist('Random Singer'));
 
 
 // ### Stretch goals
 
 // - Create a function called `search`. This function should:
-//   - Take an input parameter for a search criteria object. Create your solution based on a search object that has these properties:
+// - Take an input parameter for a search criteria object. 
+//   Create your solution based on a search object that has these properties:
 //   ```
 //   { artist: 'Ray Charles', year: 1957 }
 //   ```
-//   - The returned output from `search` should meet these requirements:
-//     - Return a new array of all items in the `collection` matching *all* of the search criteria.
-//     - If no results are found, return an empty array.
-//     - If there is no search object or an empty search object provided as input, then return all albums in the `collection`.
+// - The returned output from `search` should meet these requirements:
+// - Return a new array of all items in the `collection` matching *all* of the search criteria.
+// - If no results are found, return an empty array.
+// - If there is no search object or an empty search object provided 
+//   as input, then return all albums in the `collection`.
 
-// - Add an array of `tracks` to your album objects. Each track should have a `name` and `duration`. You will need to update the functions to support this new property:
+function search (albumArtist, year) {
+    let searchArtist = [];
+      for (let idol of collection) {
+        if (idol.artist === albumArtist && idol.yearPublished === year) {
+                searchArtist.push(idol);
+                    return searchArtist;
+        } //end of if-statement.
+    } //end of for-loop. 
+} //end of search-function.
+
+console.log(search ('BTS', 2017));
+console.log(search ('BlackPink', 2019));
+console.log(search ('BTS', 5000)); // should return as an empty array.
+console.log(search ()); // should return as collection. 
+
+
+// - Add an array of `tracks` to your album objects. Each track should have a `name` 
+//   and `duration`. You will need to update the functions to support this new property:
 //   - Update the `addToCollection` function to also take an input parameter for the array of tracks.
 //   - Update `search` to allow a `trackName` search criteria.
-//   - Update the `showCollection` function to display the list of tracks for each album with its name and duration.
+//   - Update the `showCollection` function to display the list of tracks for each album with 
+//     its name and duration.
 // ```
 //     TITLE by ARTIST, published in YEAR:
 //     1. NAME: DURATION
@@ -111,6 +152,4 @@ console.log(findByArtist (collection));
 //     TITLE by ARTIST, published in YEAR:
 //     1. NAME: DURATION
 //     2. NAME: DURATION
-// ```
-
-// > Make sure to test all your code!
+// 
